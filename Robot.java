4bd668119a9a99a2de1fafa5583e8828595086ad
yahn1609 <150978349+yahn1609@users.noreply.github.com>
2024-03-03 22:28:51 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,210 +1,3 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
-
-// package frc.robot;
-
-// import com.revrobotics.CANSparkLowLevel.MotorType;
-// import com.revrobotics.CANSparkMax;
-
-// import edu.wpi.first.util.sendable.Sendable;
-// import edu.wpi.first.wpilibj.Joystick;
-// import edu.wpi.first.wpilibj.TimedRobot;
-// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-// import com.revrobotics.CANSparkBase;
-// import com.revrobotics.CANSparkBase.IdleMode;
-
-// import edu.wpi.first.wpilibj.Timer;
-
-
-
-// /**
-//  * The VM is configured to automatically run this class, and to call the functions corresponding to
-//  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
-//  * the package after creating this project, you must also update the build.gradle file in the
-//  * project.
-//  */
-// public class Robot extends TimedRobot {
-//   /**
-//    * This function is run when the robot is first started up and should be used for any
-//    * initialization code.
-//    */
-//     // right motors
-//    private final CANSparkMax rightMotor1 = new CANSparkMax(5, MotorType.kBrushed);
-//    private final CANSparkMax rightMotor2 = new CANSparkMax(6, MotorType.kBrushed);
-   
-//    private final CANSparkMax leftMotor1 = new CANSparkMax(3, MotorType.kBrushed);
-//    private final CANSparkMax leftMotor2 = new CANSparkMax(4, MotorType.kBrushed);
-//     // this is left motors
-
-//    Joystick m_driverController = new Joystick(0);
-//    Joystick m_manipController = new Joystick(1); // delete if joystick dont work
-
-//    DifferentialDrive m_drivetrain; // if robot dont move delete this
- 
-//   CANSparkMax m_launchWheel = new CANSparkMax(7, MotorType.kBrushed); // for the outtake need to id
-//   CANSparkMax m_feedwheel = new CANSparkMax(8, MotorType.kBrushed); // intake need to id
-  
-  
-//   private static final String kNothingAuto = "do nothing";        // autonomous selection options
-//   private static final String kLaunchAndDrive = "launch drive"; // autonomous selection options
-//   private static final String kLaunch = "launch";
-//   private static final String kDrive = "drive";                         // autonomous selection options
-//   private String m_autoSelected;  // autonomous selection options
-//   private final SendableChooser<String> m_chooser = new SendableChooser<>(); // autonomous selection options
-
-
-//   // Below will be code for how much AMPS SYSTEMS CAN USE
-
-//   static final int DRIVE_CURRENT_LIMIT_A = 60;  // drive train motor limit
-
-//   static final int FEEDER_CURRENT_LIMIT_A =80; // feeder current limit
-
-//   static final double FEEDER_OUT_SPEED = 1.0; // percentage of feeder expelling note
-
-//   static final double FEEDER_IN_SPEED = -.4; // percentage of speed feeder taking in note
-
-//   static final double FEEDER_AMP_SPEED = .4; // Percent output for amp or drop note, configure based on polycarb bend
-
-//   static final int LAUNCHER_CURRENT_LIMIT_A = 80; //How many amps the launcher motor can use. lower to 60 if using neo
-
-//   static final double LAUNCHER_SPEED = 1.0; // percentage output for when lauchers is intaking and expelling note
-
-//   static final double LAUNCHER_AMP_SPEED = .17; // percentage output for scoring in amp or dropping note
-
-
-
-
-// // below if robot does not work try changing the initallizing code 
-
-//    @Override
-//   public void robotInit() {
-//     m_chooser.setDefaultOption("do nothing", kNothingAuto);         // set chooser to sendable
-//     m_chooser.addOption("launch note and drive", kLaunchAndDrive);
-//     m_chooser.addOption("launch",kLaunch);
-//     m_chooser.addOption("drive", kDrive);
-//     SmartDashboard.putData("Auto choices", (Sendable) m_chooser);
-
-//     rightMotor1.setSmartCurrentLimit(DRIVE_CURRENT_LIMIT_A);    // drive train motor amp limits
-//     rightMotor2.setSmartCurrentLimit(DRIVE_CURRENT_LIMIT_A);
-//     leftMotor1.setSmartCurrentLimit(DRIVE_CURRENT_LIMIT_A);
-//     leftMotor2.setSmartCurrentLimit(DRIVE_CURRENT_LIMIT_A);
-    
-//    rightMotor1.follow(rightMotor2);   //tells the back wheels to follow same command as first 
-//    leftMotor1.follow(leftMotor2);      // if robot drives weird change this
-
-//    rightMotor2.setInverted(true);
-//    leftMotor2.setInverted(false);  //One side of the drivetrain must be inverted, motors opposite
-
-//     m_drivetrain = new DifferentialDrive(rightMotor2, leftMotor2);
-
-//     // if launcher wheel spins wrong direction change it to true here you can add white tape to determine wheel direction
-
-//     m_feedwheel.setInverted(true);
-//     m_launchWheel.setInverted(true);
-
-//     m_feedwheel.setSmartCurrentLimit(FEEDER_CURRENT_LIMIT_A); // applies current limit to launching mechanism
-//     m_launchWheel.setSmartCurrentLimit(LAUNCHER_CURRENT_LIMIT_A);
-
-//   }
-
-//   @Override
-//   public void robotPeriodic() {}
-
-//   @Override
-//   public void autonomousInit() {}
-
-//   @Override
-//   public void autonomousPeriodic() {}
-
-//   @Override
-//   public void teleopInit() {
-//     rightMotor1.setIdleMode(IdleMode.kCoast);
-//     rightMotor2.setIdleMode(IdleMode.kCoast);
-//     leftMotor1.setIdleMode(IdleMode.kCoast);             
-//     leftMotor2.setIdleMode(IdleMode.kCoast);
-    
-//   }
-
-//   @Override
-//   public void teleopPeriodic() {
-//     if (m_manipController.getRawButton(1)) {
-//       m_launchWheel.set(LAUNCHER_SPEED);
-//     }
-//       else if(m_manipController.getRawButtonReleased(1))    // spins up the launcher wheel
-//       {
-//         m_launchWheel.set(0);
-//       }
-
-//     if (m_manipController.getRawButton(6))   // spin feeder wheel wait to spin up to full speed
-//     {
-//       m_feedwheel.set(FEEDER_OUT_SPEED);
-//     }
-//     else if(m_manipController.getRawButtonReleased(6))
-//     {
-//       m_feedwheel.set(0);
-//     }
-
-//     if(m_manipController.getRawButton(5))
-//     {
-//       m_launchWheel.set(-LAUNCHER_SPEED);
-//       m_feedwheel.set(FEEDER_IN_SPEED);
-//     }
-//     else if(m_manipController.getRawButtonReleased(5)) // while button is held spin both motor to intake note
-//     {
-//       m_launchWheel.set(0);
-//       m_feedwheel.set(0);
-//     }
-
-//     if (m_manipController.getRawButton(2))
-//     {
-//       m_feedwheel.set(FEEDER_AMP_SPEED);
-//       m_launchWheel.set(LAUNCHER_AMP_SPEED);
-
-//     }                       //While the amp button is being held, spin both motors to "spit" the note
-
-//       else if(m_manipController.getRawButtonReleased(2))  
-                                                      
-//       {
-//         m_feedwheel.set(0);
-//         m_launchWheel.set(0);
-
-
-//       }
-    
-//       m_drivetrain.arcadeDrive(-m_driverController.getY(), -m_driverController.getZ());
-
-
-//     }   // code is for only feeder and shooter as of now 2/23/2024
-
-
-  
-
-//   @Override
-//   public void disabledInit() {}
-
-//   @Override
-//   public void disabledPeriodic() {}
-
-//   @Override
-//   public void testInit() {}
-
-//   @Override
-//   public void testPeriodic() {}
-
-//   @Override
-//   public void simulationInit() {}
-
-//   @Override
-//   public void simulationPeriodic() {}
-// }
-
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
@@ -217,7 +10,13 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.SPI;
+
+
 
 
 public class Robot extends TimedRobot {
@@ -258,8 +57,8 @@ public class Robot extends TimedRobot {
    *
    * Both of the motors used on the KitBot launcher are CIMs which are brushed motors
    */
-  CANSparkMax m_launchWheel = new CANSparkMax(7, MotorType.kBrushless); // this if for shooting dont forget to label and give id 
-  CANSparkMax m_feedWheel = new CANSparkMax(8, MotorType.kBrushed); // this if for intake dont for get to label and give id
+  CANSparkMax m_launchWheel = new CANSparkMax(8, MotorType.kBrushed); // this if for shooting dont forget to label and give id 
+  CANSparkMax m_feedWheel = new CANSparkMax(7, MotorType.kBrushed); // this if for intake dont for get to label and give id
 
   /**
    * Roller Claw motor controller instance.
@@ -276,7 +75,7 @@ public class Robot extends TimedRobot {
    *
    * To determine which button on your controller corresponds to which number, open the FRC
    * driver station, go to the USB tab, plug in a controller and see which button lights up
-   * when pressed down
+   * when pressed down*
    *
    * Buttons index from 0
    */
@@ -318,7 +117,7 @@ public class Robot extends TimedRobot {
    *
    * In our testing we favored the CIM over NEO, if using a NEO lower this to 60 !!!
    */
-  static final int LAUNCHER_CURRENT_LIMIT_A = 60;
+  static final int LAUNCHER_CURRENT_LIMIT_A = 80;
 
   /**
    * Percent output to run the launcher when intaking AND expelling note
@@ -444,7 +243,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
-
+    
     leftRear.setIdleMode(IdleMode.kBrake);
     leftFront.setIdleMode(IdleMode.kBrake);
     rightRear.setIdleMode(IdleMode.kBrake);
@@ -469,7 +268,7 @@ public class Robot extends TimedRobot {
     }
     else if(m_autoSelected == kDrive)
     {
-      AUTO_LAUNCHER_SPEED = 0;                          // Launcher speed
+      AUTO_LAUNCHER_SPEED = 1;                          // Launcher speed
     }
     else if(m_autoSelected == kNothingAuto)
     {
@@ -483,41 +282,48 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-
     double timeElapsed = Timer.getFPGATimestamp() - autonomousStartTime;
-
-    /*
-     * Spins up launcher wheel until time spent in auto is greater than AUTO_LAUNCH_DELAY_S
-     *
-     * Feeds note to launcher until time is greater than AUTO_DRIVE_DELAY_S
-     *
-     * Drives until time is greater than AUTO_DRIVE_DELAY_S + AUTO_DRIVE_TIME_S
-     *
-     * Does not move when time is greater than AUTO_DRIVE_DELAY_S + AUTO_DRIVE_TIME_S
-     */
+    
     if(timeElapsed < AUTO_LAUNCH_DELAY_S)
     {
-      m_launchWheel.set(AUTO_LAUNCHER_SPEED);
+      m_feedWheel.set(-LAUNCHER_SPEED);
+      m_launchWheel.set(FEEDER_IN_SPEED);
+      
+      
       m_drivetrain.arcadeDrive(0, 0);
 
     }
     else if(timeElapsed < AUTO_DRIVE_DELAY_S)
     {
       m_feedWheel.set(AUTO_LAUNCHER_SPEED);
+      
+      
+      
       m_drivetrain.arcadeDrive(0, 0);
     }
     else if(timeElapsed < AUTO_DRIVE_DELAY_S + AUTO_DRIVE_TIME_S)
     {
-      m_launchWheel.set(0);
-      m_feedWheel.set(0);
+      m_launchWheel.set(1);
+      m_feedWheel.set(1);
       m_drivetrain.arcadeDrive(AUTO_DRIVE_SPEED, 0);
     }
     else
     {
       m_drivetrain.arcadeDrive(0, 0);
     }
+      
+
+        
+    }
+    
+
+
+    
+
+
+    
     /* For an explanation on differintial drive, squaredInputs, arcade drive and tank drive see the bottom of this file */
-  }
+  
 
   /** This function is called once when teleop is enabled. */
   @Override
@@ -547,12 +353,12 @@ public class Robot extends TimedRobot {
     /*
      * Spins up the launcher wheel
      */
-    if (m_manipController.getRawButton(1)) {
-      m_launchWheel.set(LAUNCHER_SPEED);                    // need to see what rawbutton 1 is 
+    if (m_manipController.getRawButton(5)) {
+      m_feedWheel.set(LAUNCHER_SPEED);                    // need to see what rawbutton 1 is 
     }
-    else if(m_manipController.getRawButtonReleased(1)) // spins up launcher wheel which is x
+    else if(m_manipController.getRawButtonReleased(5)) // spins up launcher wheel which is lb
     {
-      m_launchWheel.set(0);
+      m_feedWheel.set(0);
     }
 
     /*
@@ -560,25 +366,25 @@ public class Robot extends TimedRobot {
      */
     if (m_manipController.getRawButton(6))
     {
-      m_feedWheel.set(FEEDER_OUT_SPEED);                        // need to see what raw button 6 is
+      m_launchWheel.set(FEEDER_OUT_SPEED);                        // need to see what raw button 6 is
     }
     else if(m_manipController.getRawButtonReleased(6))  //rb should outtake which is bottom wheel
     {
-      m_feedWheel.set(0);
+      m_launchWheel.set(0);
     }
 
     /*
      * While the button is being held spin both motors to intake note
      */
-    if(m_manipController.getRawButton(5))   
+    if(m_manipController.getRawButton(1))   
     {
-      m_launchWheel.set(-LAUNCHER_SPEED);                                 // intake is 2 buttons
-      m_feedWheel.set(FEEDER_IN_SPEED);
+      m_feedWheel.set(-LAUNCHER_SPEED);                                 // intake is 2 buttons
+      m_launchWheel.set(FEEDER_IN_SPEED);
     }
-    else if(m_manipController.getRawButtonReleased(5)) //LB for both motors
+    else if(m_manipController.getRawButtonReleased(1)) //1 for both motors
     {
-      m_launchWheel.set(0);
       m_feedWheel.set(0);
+      m_launchWheel.set(0);
     }
 
     /*
@@ -589,8 +395,8 @@ public class Robot extends TimedRobot {
      */
     if(m_manipController.getRawButton(2))         // A spins both motor to spit note out lower speed                           
     {
-      m_feedWheel.set(FEEDER_AMP_SPEED);
-      m_launchWheel.set(LAUNCHER_AMP_SPEED);
+      m_launchWheel.set(FEEDER_AMP_SPEED);
+      m_feedWheel.set(LAUNCHER_AMP_SPEED);
     }
     else if(m_manipController.getRawButtonReleased(2))
     {
